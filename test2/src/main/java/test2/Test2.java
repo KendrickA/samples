65968f420f505b008fce628bc9adef8156eea27e
Andy Kendrick <andrewkendrick45@gmail.com>
2018-01-29 16:25:04 +0000

    vb

---
+++ test2/src/main/java/test2/Test2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test2;
+
+/**
+ * @author user
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+
+	}
+
+}
